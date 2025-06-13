df918b7b67dcf0a599645058bd73d232860f6144
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2025-06-13 15:28:32 +0700

    update build.gradle.kts: - add mockk

--- source code/build.gradle.kts
+++ source code/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    testImplementation("io.mockk:mockk:1.14.2")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
