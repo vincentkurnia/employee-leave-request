60218b42e63e2223e2fc228bedd46d6fee044e3a
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2025-06-13 13:24:24 +0700

    using getCurrentDateTime in ErrorResponseDto

--- source code/src/main/kotlin/com/example/employee/leave/request/dto/ErrorResponseDto.kt
+++ source code/src/main/kotlin/com/example/employee/leave/request/dto/ErrorResponseDto.kt
@@ -1,9 +1,10 @@
 package com.example.employee.leave.request.dto
 
+import com.example.employee.leave.request.utils.getCurrentDateTime
 import java.time.LocalDateTime
 
 data class ErrorResponseDto(
-    val timestamp: LocalDateTime = LocalDateTime.now(),
+    val timestamp: LocalDateTime = getCurrentDateTime(),
     val status: Int,
     val message: String?,
     val path: String
