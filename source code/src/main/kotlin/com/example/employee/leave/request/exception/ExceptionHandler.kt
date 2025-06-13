5e338a0a6453c94b83df1c1590267afe52814d5b
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2025-06-13 10:18:30 +0700

    add exception handler: - employee not found

---
+++ source code/src/main/kotlin/com/example/employee/leave/request/exception/ExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.example.employee.leave.request.exception
+
+import com.example.employee.leave.request.dto.ErrorResponseDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.server.ServerWebExchange
+
+@RestControllerAdvice
+class ExceptionHandler {
+    @ExceptionHandler(EmployeeNotFoundException::class)
+    fun handleEmployeeNotFoundException(e: EmployeeNotFoundException, exchange: ServerWebExchange): ResponseEntity<ErrorResponseDto> {
+        return ResponseEntity(e.toErrorResponseDto(exchange), e.statusCode)
+    }
+
+    private fun CustomException.toErrorResponseDto(exchange: ServerWebExchange): ErrorResponseDto {
+        return ErrorResponseDto(
+            status = this.statusCode.value(),
+            message = this.message,
+            path = exchange.request.path.toString()
+        )
+    }
+}
