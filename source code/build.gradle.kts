ec345ee4fb18c1e740c3281b3d42b82d7c44aba9
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2025-06-13 14:15:56 +0700

    update build.gradle.kts: - add micrometer context-propagation

--- source code/build.gradle.kts
+++ source code/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.micrometer:context-propagation")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
